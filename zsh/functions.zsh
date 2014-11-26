26695673fc390e2dfc6bf1e5e5e13de8dcef9c02
Zach Olauson <zmolauson@gmail.com>
2014-11-26 11:17:06 -0800

    add function for printing tmux color codes

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -30,6 +30,12 @@ function set-git-email() {
 
 add-zsh-hook chpwd set-git-email
 
+function tmux-colors() {
+  for i in {0..255} ; do
+    printf "\x1b[38;5;${i}mcolour${i}\n"
+  done
+}
+
 function slurp() {
   cat $1 | pbcopy
 }
